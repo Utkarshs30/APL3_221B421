6ef695c168ec7c6b512874ce882622cead5627c7
Utkarshs30 <115998398+Utkarshs30@users.noreply.github.com>
2025-02-20 12:10:28 +0530

    PROBLEM3

---
+++ LAB_EXERCISE3/RedHeadDuck.java
@@ -0,0 +1,16 @@
+// RedHeadDuck.java
+public class RedHeadDuck implements Duck, Flyable, Quackable {
+
+    public void swim() {
+        System.out.println("RedHead Duck is swimming gracefully!");
+    }
+
+    public void fly() {
+        System.out.println("RedHead Duck is flying!");
+    }
+
+
+    public void quack() {
+        System.out.println("RedHead Duck quacks!");
+    }
+}
